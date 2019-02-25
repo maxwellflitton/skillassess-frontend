75c9a4886c7a6de345d1a6594910cedf95c08341
Maxwell Flitton <maxwellflitton@gmail.com>
2019-02-25 21:40:25 +0000

    basics of Quiz Detail working

--- skill-assess/src/components/QuizDetail.js
+++ skill-assess/src/components/QuizDetail.js
@@ -10,23 +10,52 @@ class QuizDetail extends Component {
 	state = {
 	    quizType: '',
 	    quizName: 'Basic Anatomy',
-		completedTime: '0',
+		completedTime: 0,
+		questionNumber : 0,
+		questions : ["this is a placeholder", "this is another placeholder"],
+		displayQuestion: null, 
+		total: null 
 	}
 
-//    function GoForward{
-//
-//    }
+	startTest = () => {
+		this.setState({total: this.state.questions.length, displayQuestion: this.state.questions[0]})
+		// here is where we will do the API call to get the questions in order to fill in the list of questions
+	}
+
+	submitAnswer = () => {
+		// here we will update the displayQuestion answer attribute  
+	}
+
+	moveForward = () => {
+		if (this.state.questionNumber === this.state.total - 1) {
+
+		} else {
+			this.setState({questionNumber: this.state.questionNumber + 1})
+		    this.setState({displayQuestion: this.state.questions[this.state.questionNumber]})
+		}
+	}
+
+	moveBackward = () => {
+		if (this.state.questionNumber === 0) {
+
+		} else {
+			this.setState({questionNumber: this.state.questionNumber - 1})
+		    this.setState({displayQuestion: this.state.questions[this.state.questionNumber]})
+		}
+	}
 
 	render() {
         return (
         <div className="questionContainer">
+        <button className="questionNavPost" to={"/"} onClick={this.startTest}>Start</button>
             <h1> {this.state.quizName} </h1>
             <div className="question">
+            {/* A JSX comment */}
             </div>
             <Question 
-            question="" />
-            <Link className="questionNavPre" to={"/quizes"}>Previous</Link>
-            <Link className="questionNavPost" to={"/"}>Next</Link>
+            question={this.state.displayQuestion} />
+            <button className="questionNavPre" to={"/"} onClick={this.moveBackward}>Previous</button>
+            <button className="questionNavPost" to={"/"} onClick={this.moveForward}>Next</button>
         </div>
 		)
 	}
